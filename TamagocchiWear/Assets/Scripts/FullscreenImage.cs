151e46189572fbfb375020fb9a10751f56755c53
Daniel <danELhombre@gmail.com>
2019-04-03 23:38:30 -0700

    Initial commit

---
+++ TamagocchiWear/Assets/Scripts/FullscreenImage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullscreenImage : MonoBehaviour
+{
+    void Awake()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+        Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+
+        Vector2 scale = transform.localScale;
+
+        // Landscape (or equal)
+        if (cameraSize.x >= cameraSize.y)
+            scale *= cameraSize.x / spriteSize.x;
+            scale *= cameraSize.y / spriteSize.y;
+
+        // Optional
+        transform.position = Vector2.zero;
+        transform.localScale = scale;
+    }
+}
---
+++ TamagocchiWear/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Keep the screen from sleeping.
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
